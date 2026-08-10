4ef950af9973edb0c304e86f16ca7ec576e19ff6
Fives <www.phengang728@gmail.com>
2026-08-10 10:15:51 +0700

    Javascript for Calcutor

--- js/index.js
+++ js/index.js
@@ -1,2 +1,20 @@
-console.log("Hello World!");
-console.log("I am learning how to Javascript!")
+// CALCULATOR PROGRAM
+
+const display = document.getElementById("display");
+
+function appendToDisplay(input){
+    display.value += input;
+}
+
+function clearDisplay(){
+    display.value = "";
+}
+
+function calculate(){
+    try{
+        display.value = eval(display.value);
+    }
+    catch(error){
+        display.value = "Error";
+    }
+}
