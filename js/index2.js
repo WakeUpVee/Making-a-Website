f04a7e80c325d0745b83637238865d5235e0cd37
Fives <www.phengang728@gmail.com>
2026-09-07 14:10:11 +0700

    Index 2

---
+++ js/index2.js
@@ -0,0 +1,23 @@
+console.log('Hello World!');
+console.log('What is the up gang!');
+console.log('This is Ung Phengangs CV');
+
+let x = 100;
+let y = 200;
+
+let age = 24;
+let price = 1.99
+
+let firstName = "Jeff";
+
+let username;
+
+console.log(age);
+console.log(price);
+console.log(x);
+
+console.log(`You are ${age} years old!`);
+console.log(`The price is $${price} for the hotdog!`);
+
+console.log(`My name is ${firstName}`);
+
